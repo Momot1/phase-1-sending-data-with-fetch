a84c991b14019b3f118fc0f2df8b752daf0d900c
Mark Momot <markmomot03@gmail.com>
2021-10-20 14:53:24 -0400

    commit tree 0d9c06f8a3f102188b95326817c44296c1c721a4

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name,  email){
+    const body = document.querySelector('body')
+    const p = document.createElement('p')
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        p.textContent = `${data.id}`
+        body.appendChild(p)
+    })
+    .catch(error => {
+        p.textContent = `${error}`
+        body.appendChild(p)
+    })
+}
